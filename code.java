8b496858a7040ee23b12f6441c9de15c3f299f9d
Aviral Gupta <66764219+noob-aViral@users.noreply.github.com>
2020-10-31 17:50:02 +0530

    Update code.java

--- code.java
+++ code.java
@@ -1,23 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 class mainpage{
 	public void display()
 	{
 		JFrame frame = new JFrame("FILL DETAILS");
-		JLabel lblName, lblEmail, lblId, lblContact, lblTime, lblHostName, lblHostEmail, lblHost, lblIdType, lblVenue, lblStayingPlace, lblDays; 
+		JLabel lblVisitor, lblName, lblEmail, lblId, lblContact, lblTime, lblHostName, lblHostEmail, lblHost, lblIdType, lblVenue, lblStayingPlace, lblDays; 
 		JTextField txtName, txtEmail, txtId, txtContact, txtHostName, txtHostEmail, txtDays;
 		JComboBox txtIdType, txtStayingPlace;
+		JButton btnSubmit, btnCancel;
+		Color background = new Color(50, 50, 50);
+		Color foreground = new Color(65, 105, 225);
+		Color purple = new Color(115, 25, 150);
+		JPanel mainpanel = new JPanel(new GridLayout(1,2,10,20));
 
 		//**************************************************************************************************************
 		// USER DETAILS
-			lblName = 	new JLabel("Name: ");
-			lblEmail = 	new JLabel("Email: ");
-			lblIdType = new JLabel("ID Type:");
-			lblId = new JLabel("ID: ");
-			lblContact = new JLabel("Contact no.: ");
-			lblTime = new JLabel("Choose hours of stay: ");		
+			JPanel panel = new JPanel(new GridLayout(15,2,10,10));
+			lblVisitor = new JLabel("VISITOR DETAILS", JLabel.CENTER);
+			lblVisitor.setFont(new Font("Courier New", Font.BOLD, 16));
+			lblName = new JLabel("Name: ", JLabel.TRAILING);
+			lblEmail = new JLabel("Email: ", JLabel.TRAILING);
+			lblIdType = new JLabel("ID Type:", JLabel.TRAILING);
+			lblId = new JLabel("ID: ", JLabel.TRAILING);
+			lblContact = new JLabel("Contact no.: ", JLabel.TRAILING);
+			lblTime = new JLabel("Choose hours of stay: ", JLabel.TRAILING);		
 
 			txtName = new JTextField(60);
-			txtEmail = new JTextField("abc@iiita.ac.in", 60);
+			txtEmail = new JTextField("@iiita.ac.in", 60);
 			txtId = new JTextField(60);
 			txtContact = new JTextField(60);
 
@@ -25,73 +34,78 @@ class mainpage{
 			txtIdType = new JComboBox(txtIdTypeOptions);
 			txtIdType.setSelectedIndex(0);
 
-			int x=30, y=30, height=30, width=150;
-			lblName.setBounds(x,y,width,height);				txtName.setBounds(x+160,y,width+100,height);
-			lblEmail.setBounds(x,y+40,width,height);			txtEmail.setBounds(x+160,y+40,width+100,height);
-			lblIdType.setBounds(x,y+80,width,height);			txtIdType.setBounds(x+160,y+80,width+100,height);
-			lblId.setBounds(x,y+120,width,height);				txtId.setBounds(x+160,y+120,width+100,height);
-			lblContact.setBounds(x,y+160,width,height);			txtContact.setBounds(x+160,y+160,width+100,height);
-			
-			frame.add(lblName);		frame.add(txtName);
-			frame.add(lblEmail);		frame.add(txtEmail);
-			frame.add(lblIdType);		frame.add(txtIdType);	
-			frame.add(lblId);		frame.add(txtId);
-			frame.add(lblContact);		frame.add(txtContact);
+			panel.add(lblVisitor);		panel.add(new JLabel("	"));
+			panel.add(lblName);			panel.add(txtName);
+			panel.add(lblEmail);		panel.add(txtEmail);
+			panel.add(lblIdType);		panel.add(txtIdType);	
+			panel.add(lblId);			panel.add(txtId);
+			panel.add(lblContact);		panel.add(txtContact);
 		//**************************************************************************************************************
 		
 		//**************************************************************************************************************
 		// HOST DETAILS
-			lblHost = new JLabel("Host Details");
-			lblHostName = new JLabel("Host Name: ");
-			lblHostEmail = new JLabel("Host Email: ");
+			lblHost = new JLabel("HOST DETAILS", JLabel.CENTER);
+			lblHost.setFont(new Font("Arial", Font.BOLD, 16));
+			lblHostName = new JLabel("Host Name: ", JLabel.TRAILING);
+			lblHostEmail = new JLabel("Host Email: ", JLabel.TRAILING);
 
 			txtHostName = new JTextField(60);
-			txtHostEmail = new JTextField(60);
+			txtHostEmail = new JTextField("@gmail.com", 60);
 			
-			lblHost.setBounds(x, y+210, width, height);
-			lblHostName.setBounds(x,y+240,width,height);		txtHostName.setBounds(x+160,y+240,width+100,height);
-			lblHostEmail.setBounds(x,y+280,width,height);		txtHostEmail.setBounds(x+160,y+280,width+100,height);
-			
-			frame.add(lblHost);
-			frame.add(lblHostName);		frame.add(txtHostName);
-			frame.add(lblHostEmail);	frame.add(txtHostEmail);
+			panel.add(lblHost);				panel.add(new Label("	"));
+			panel.add(lblHostName);			panel.add(txtHostName);
+			panel.add(lblHostEmail);		panel.add(txtHostEmail);
 		//**************************************************************************************************************
 		
 		//**************************************************************************************************************
 		// VENUE DETAILS
-			lblVenue = new JLabel("Venue Details");
-			lblStayingPlace = new JLabel("Staying Place: ");
-			lblDays = new JLabel("For How Many Days?");
+			lblVenue = new JLabel("VENUE DETAILS", JLabel.CENTER);
+			lblVenue.setFont(new Font("", Font.BOLD, 16));
+			lblStayingPlace = new JLabel("Staying Place: ", JLabel.TRAILING);
+			lblDays = new JLabel("For How Many Days?", JLabel.TRAILING);
 
 			String txtAllPlaces[] = {"BH-1,2", "BH-3,4", "BH-5", "GH-1,2,3"};
 			txtStayingPlace = new JComboBox(txtAllPlaces);
 			txtStayingPlace.setSelectedIndex(2);
 			txtDays = new JTextField(60);
 
-			lblVenue.setBounds(x, y+330, width, height);
-			lblStayingPlace.setBounds(x, y+360, width, height);		txtStayingPlace.setBounds(x+160, y+360, width+100, height);
-			lblDays.setBounds(x, y+400, width, height);			txtDays.setBounds(x+160, y+400, width+100, height);
-
-			frame.add(lblVenue);
-			frame.add(lblStayingPlace);		frame.add(txtStayingPlace);
-			frame.add(lblDays);			frame.add(txtDays);
+			panel.add(lblVenue);				panel.add(new Label("	"));
+			panel.add(lblStayingPlace);			panel.add(txtStayingPlace);
+			panel.add(lblDays);					panel.add(txtDays);
 		//**************************************************************************************************************	
 
 		//**************************************************************************************************************	
-		// SUBMIT BUTTON
-			submit = new JButton("Submit");
-			submit.setBackground(purple);
-			submit.setForeground(Color.white);
-			submit.setBounds(150, 480, width, height);
-			frame.add(submit);
+		// SUBMIT AND CANCEL BUTTON
+			JPanel panel2 = new JPanel(new GridLayout(1,2,0,0));
+			btnCancel = new JButton("Cancel");
+			btnCancel.setBackground(new Color(0xf6, 0xef, 0xd0));
+
+			btnSubmit = new JButton("Submit");
+			btnSubmit.setBackground(purple);
+			btnSubmit.setForeground(Color.white);
+		
+			panel2.add(btnCancel);			panel2.add(btnSubmit);
+			panel.add(new JLabel(" "));		panel.add(panel2);
+			panel.setBackground(new Color(219,207,92));
 		//**************************************************************************************************************	
 		
-		frame.setSize(600,600);
-		frame.setLayout(null);//using no layout managers  
-		//frame.getContentPane().setBackground(background);
-		//frame.getContentPane().setForeground(foreground);
+		//**************************************************************************************************************	
+		// CAMERA
+			JPanel panel1 = new JPanel(new GridLayout(1,1));
+			panel1.setBackground(new Color(48,25,52));
+			JLabel lblCamera = new JLabel("CAMERA");
+			lblCamera.setForeground(new Color(219,207,92));
+			panel1.add(lblCamera);
+
+		//**************************************************************************************************************	
+		mainpanel.add(panel);
+		mainpanel.add(panel1);
+		frame.add(mainpanel);
+		frame.setSize(600,600);//400 width and 500 height  
+		frame.getContentPane().setBackground(background);
+		frame.getContentPane().setForeground(foreground);
 		frame.setVisible(true);//making the frame visible
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 	}
 }
 
@@ -101,3 +115,47 @@ public class code{
 		obj.display();
 	}
 }
+			
+/*
+public class code implements ActionListener,ItemListener
+{
+	public void init()
+	{
+		submit.frame.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent ae)
+	{
+		if( isName(txtName.getText())		&& 
+			isEmail(txtEmail.getText())		&&
+			isContact(txtContact.getText()))
+		{
+			txtArea.setText("NAME: "+txtName.getText()+"\n"+"E-MAIL: "+txtEmail.getText()+"\n"+"ID: "+txtId.getText()+"\n"+"CONTACT: "+txtContact.getText());	
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Invalid Input");
+		}
+	}
+	public static boolean isName(String input)
+	{
+		return true;
+		//return Pattern.matches("^[A-Za-z] [A-Za-z]$", input);
+	}
+	public static boolean isContact(String input)
+	{
+		return true;
+		//return Pattern.matches("^[A-Za-z] [A-Za-z]$", input);
+	}
+	public static boolean isEmail(String input)
+	{
+		return true;
+		//return Pattern.matches("??????????@iiita.ac.in$", input);
+	}
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+
+	}
+}
+*/
